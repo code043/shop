07c93658fd307d6b8192b7153cf38f3c9bbdc6c3
code043 <procoder043@gmail.com>
2026-05-28 17:06:01 -0300

    feat: hero

---
+++ src/components/hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className="bg-[url('/cover.jpeg')] bg-cover bg-center h-100 flex items-center justify-center">
+      <h1 className="text-gray-900 text-center text-4xl font-bold">FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+    </div>
+  )
+}
